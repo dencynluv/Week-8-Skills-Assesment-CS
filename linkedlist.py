37c1dd8b4440c42d2c90647a86bf19198f3fe628
Cynthia Soto <sotocyn@gmail.com>
2016-05-29 20:44:13 -0700

    CS Data structs assessment

---
+++ linkedlist.py
@@ -0,0 +1,115 @@
+# Linked list with Node/LinkedList classes
+
+
+class Node(object):
+    """Node in a linked list."""
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return "<Node %s>" % self.data
+
+
+class LinkedList(object):
+    """Linked List using head and tail."""
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_node(self, data):
+        """Add node with data to end of list."""
+
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+
+        if self.tail is not None:  #meaning its pointing to a node
+            self.tail.next = new_node #point the tail to the new node
+
+        self.tail = new_node
+
+    def remove_node_by_index(self, index):
+        """Remove node with given index."""
+
+        prev = None
+        node = self.head
+        i = 0
+
+        while (node is not None) and (i < index):
+            prev = node
+            node = node.next
+            i += 1
+
+        if prev is None:
+            self.head = node.next
+        else:
+            prev.next = node.next
+
+    def find_node(self, data):
+        """Is a matching node in the list?"""
+
+        current = self.head
+
+        while current is not None:
+            if current.data == data:
+                return True
+
+            current = current.next
+
+        return False
+
+    def print_list(self):
+        """Print all items in the list::
+
+            >>> ll = LinkedList()
+            >>> ll.add_node('dog')
+            >>> ll.add_node('cat')
+            >>> ll.add_node('fish')
+
+            >>> ll.print_list()
+            dog
+            cat
+            fish
+        """
+
+        # FIXME
+
+        current = self.head #initialize temp
+
+        while current is not None: #while there is a node
+            print current.data #print the data in that node
+            current = current.next # set the pointer to the next node 
+            # and loop again until there are no more nodes (ll becomes false)
+
+
+    def get_node_by_index(self, idx):
+        """Return a node with the given index::
+
+            >>> ll = LinkedList()
+            >>> ll.add_node('dog')
+            >>> ll.add_node('cat')
+            >>> ll.add_node('fish')
+
+            >>> ll.get_node_by_index(0)
+            <Node dog>
+
+            >>> ll.get_node_by_index(2)
+            <Node fish>
+        """
+
+        # FIXME
+
+        current = self.head #Initialize temp
+
+        count = 0 #Index of the current node
+
+        while current: #loop until end of linked list is not reached
+            if count == idx: #if the count matches the index passed
+                return current.data #return the data at that node
+
+            count += 1 #set the count to increment by 1 each time it goes through the loop.
+            current = current.next #set current to the next node
---
+++ stack.py
@@ -0,0 +1,158 @@
+class StackEmptyError(IndexError):
+    """Attempt to pop an empty stack."""
+
+
+class Stack(object):
+    """LIFO stack.
+
+    Implemented using a Python list; since stacks just need
+    to pop and push, a list is a good implementation, as
+    these are O(1) for native Python lists. However, in cases
+    where performance really matters, it might be best to
+    use a Python list directly, as it avoids the overhead
+    of a custom class.
+
+    Or, for even better performance (& typically smaller
+    memory footprint), you can use the `collections.deque`
+    object, which can act like a stack.
+
+    (We could also write our own LinkedList class for a
+    stack, where we push things onto the head and pop things
+    off the head (effectively reversing it), but that would be less
+    efficient than using a built-in Python list or a
+    `collections.deque` object)
+    """
+
+    def __init__(self):
+        self._list = []
+
+    def __repr__(self):
+        if not self._list:
+            return "<Stack (empty)>"
+        else:
+            return "<Stack tail=%s length=%d>" % (
+                self._list[-1], len(self._list))
+
+    def push(self, item):
+        """Add item to end of stack."""
+
+        self._list.append(item)
+
+    def pop(self):
+        """Remove item from end of stack and return it."""
+
+        if not self._list:
+            raise StackEmptyError()
+
+        return self._list.pop()
+
+    def __iter__(self):
+        """Allow iteration over list.
+
+        __iter__ is a special method that, when defined,
+        allows you to loop over a list, so you can say things
+        like "for item in my_stack", and it will pop
+        successive items off.
+        """
+
+        while True:
+            try:
+                yield self.pop()
+            except StackEmptyError:
+                raise StopIteration
+
+    def length(self):
+        """Return length of stack::
+
+            >>> s = Stack()
+            >>> s.length()
+            0
+
+            >>> s.push("dog")
+            >>> s.push("cat")
+            >>> s.push("fish")
+
+            >>> s.length()
+            3
+        """
+
+        # FIXME
+
+        return len(self._list)
+
+
+    def empty(self):
+        """Empty stack
+
+            >>> s = Stack()
+            >>> s.push("dog")
+            >>> s.push("cat")
+            >>> s.push("fish")
+
+            >>> s.length()
+            3
+
+            >>> s.empty()
+
+            >>> s.length()
+            0
+        """
+
+        # FIXME
+
+        # 1 Approach
+        # for i in self. _list:
+        #     empty_list = self._list.pop([i])
+
+        # return empty_list
+
+        # Another approach
+        # empty_list = self._list.clear()
+
+        # return empty_list
+
+        # Another approach
+        # del self._list[:]
+        # return self._list
+
+        # Another approach
+        # for i in range(len(self._list)):
+        #     empty = i.pop()
+
+        # return empty
+
+        # Another approach
+        # return len(self._list).pop()
+
+        # Another approach
+        # return pop(len(self._list))
+
+        # Another attmept: still not working. returns 4 and
+        # when check length() it returns 2 (deletes fish but not the rest)
+        # return self._list[:]
+
+        # Works
+        self._list = []
+        empty_list = self._list
+
+        return empty_list
+
+    def is_empty(self):
+        """Is stack empty?
+
+            >>> s = Stack()
+
+            >>> s.is_empty()
+            True
+
+            >>> s.push("dog")
+            >>> s.push("cat")
+            >>> s.push("fish")
+
+            >>> s.is_empty()
+            False
+        """
+
+        # FIXME
+
+        return not self._list
